a1883269de076fde2e2a798bd3814a8062bdf6de
Naim Efe <naimefe@outlook.com.tr>
2021-04-22 17:51:09 +0300

    edit satırı eklendi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Merhaba Git");
+            Console.WriteLine("edit");
         }
     }
 }
